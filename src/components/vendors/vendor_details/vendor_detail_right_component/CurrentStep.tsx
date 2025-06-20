413cd955023e078ebdfa138052dc91b19483b8a9
shaanarshaqu <shaanarshaqu123@gmail.com>
2025-06-20 16:15:07 +0530

    fixes

--- src/components/vendors/vendor_details/vendor_detail_right_component/CurrentStep.tsx
+++ src/components/vendors/vendor_details/vendor_detail_right_component/CurrentStep.tsx
@@ -42,96 +42,58 @@ const CurrentStep: React.FC<{ step: IStep; trigger: () => void }> = ({
   step,
   trigger
 }) => {
-  const { requestId } = useParams();
   const currentUserId = Cookies.get("userId") || "";
-  const [selectedAction, setSelectedAction] = useState<"approved" | "rejected" | "under_clarification">("approved");
+  const [selectedAction, setSelectedAction] = useState<"approved" | "rejected">("approved");
   const [approveComment, setApproveComment] = useState("");
-  const [rejectComment, setRejectComment] = useState("");
   const [showLoaderOnButton, setShowLoaderOnButton] = useState<boolean>(false);
-  const [clarificationRequest, setClarificationRequest] = useState({
-    clarificationRequest: "",
-    clarificationUserId: "",
-    clarificationUserEmail: "",
-    clarificationUserName: "",
-  });
+
   const [errors, setErrors] = useState({
-    approveComment: "",
-    rejectComment: "",
-    clarificationRequest: "",
-    clarificationUser: ""
+    approveComment: ""
   });
 
-  const handleActionClick = (action: "approved" | "rejected" | "under_clarification") => {
+  const handleActionClick = (action: "approved" | "rejected") => {
     setSelectedAction(action);
+    setErrors({
+      approveComment: ""
+    });
   };
 
   const handleSubmit = async () => {
-    if (!requestId || !selectedAction) {
-      console.error("Missing required data");
-      return;
-    }
-
-    const newErrors = { approveComment: "", rejectComment: "", clarificationRequest: "", clarificationUser: "" };
-    let hasError = false;
-
-    // if (selectedAction === "approved" && !approveComment.trim()) {
-    //   newErrors.approveComment = "Comments are required for approval.";
-    //   hasError = true;
+    // if (!requestId || !selectedAction) {
+    //   console.error("Missing required data");
+    //   return;
     // }
 
-    if (selectedAction === "rejected" && !rejectComment.trim()) {
-      newErrors.rejectComment = "Comments are required for rejection.";
+    const newErrors = { approveComment: ""};
+    let hasError = false;
+
+    if (selectedAction === "approved" && !approveComment.trim()) {
+      newErrors.approveComment = "Comments are required for approval.";
       hasError = true;
     }
 
-    if (selectedAction === "under_clarification") {
-      if (!clarificationRequest.clarificationUserId) {
-        newErrors.clarificationUser = "Please select a user for clarification.";
-        hasError = true;
-      }
-      if (!clarificationRequest.clarificationRequest.trim()) {
-        newErrors.clarificationRequest = "Clarification request is required.";
-        hasError = true;
-      }
+    if (selectedAction === "rejected" && !approveComment.trim()) {
+      newErrors.approveComment = "Comments are required for rejection.";
+      hasError = true;
     }
 
     setErrors(newErrors);
 
     if (hasError) return;
 
-    let responseBody: IApproverResponse = {
-      capexRequestId: requestId,
+    let responseBody = {
+      // capexRequestId: requestId,
       status: selectedAction,
       comments: "", // Default empty comment
     };
 
     if (selectedAction === "approved") {
-      responseBody.comments = approveComment;
+
     } else if (selectedAction === "rejected") {
-      responseBody.comments = rejectComment;
-    } else if (selectedAction === "under_clarification") {
-      responseBody.clarificationUserId = clarificationRequest.clarificationUserId;
-      responseBody.clarification = clarificationRequest.clarificationRequest;
+
     }
 
-    // try {
-    //   setShowLoaderOnButton(true);
-    //   const response = await postApproverResponse(responseBody);
-    //   if (response) {
-    //     setShowLoaderOnButton(false);
-    //     trigger();
-    //     notification.success({
-    //       message:"Response submitted successfully"
-    //     })
-    //   }
-    // } catch (error: any) {
-    //   notification.error({
-    //     message: "Response error",
-    //     description: error.message
-    //   });
-    // } finally {
-    //   setShowLoaderOnButton(false);
-    // }
+
   };
 
   return (
@@ -197,66 +159,7 @@ const CurrentStep: React.FC<{ step: IStep; trigger: () => void }> = ({
                 </button>
               </>
             )}
-            {/* <button
-              type="button"
-              disabled={showLoaderOnButton}
-              className={`px-4 py-1.5 text-sm rounded ${selectedAction === "under_clarification" ? "text-white bg-[#E79937]" : "bg-[#E799371A]"}`}
-              onClick={() => handleActionClick("under_clarification")}
-            >
-              Send for clarification
-            </button> */}
           </div>
-
-          {/* Conditional Form Fields */}
-          {/* {selectedAction === "under_clarification" && (
-            <div className="rounded-lg mb-3">
-              <div className="mb-3">
-                <label htmlFor="clarification-request" className="text-sm text-gray-500">
-                  Clarification from
-                </label>
-                <SelectField
-                  id="owner"
-                  label=""
-                  style="w-full"
-                  value={clarificationRequest?.clarificationUserEmail || "Select a user"}
-                  options={(ownerDetails || []).map((x) => ({
-                    label: (
-                      <div>
-                        <span className="text-md font-medium">{x.ownerName}</span>
-                        <br />
-                        <span className="text-sm text-gray-500">{x.ownerEmail}</span>
-                      </div>
-                    ),
-                    value: JSON.stringify([x.ownerId, x.ownerEmail, x.ownerName]),
-                  }))}
-                  onChange={(selectedValue) => {
-                    const [clarificationUserId, clarificationUserEmail, clarificationUserName] = JSON.parse(selectedValue);
-                    setClarificationRequest({
-                      clarificationUserId,
-                      clarificationUserEmail,
-                      clarificationUserName,
-                      clarificationRequest: "",
-                    });
-                  }}
-                />
-                {errors.clarificationUser && <p className="text-red-500 text-xs mb-1">{errors.clarificationUser}</p>}
-              </div>
-
-              <div className="mb-3">
-                <label htmlFor="clarification-request" className="text-sm text-gray-500">
-                  Clarification request
-                </label>
-                <textarea
-                  id="clarification-request"
-                  value={clarificationRequest.clarificationRequest}
-                  onChange={(e) => setClarificationRequest({ ...clarificationRequest, clarificationRequest: e.target.value })}
-                  className="w-full border border-gray-200 rounded p-3 text-sm h-24 resize-none focus:outline-none focus:ring-1 focus:ring-blue-500"
-                  placeholder="Enter your clarification request"
-                />
-                {errors.clarificationRequest && <p className="text-red-500 text-xs mb-1">{errors.clarificationRequest}</p>}
-              </div>
-            </div>
-          )} */}
 
           {selectedAction === "approved" && (
             <div className="mb-2">
@@ -281,12 +184,12 @@ const CurrentStep: React.FC<{ step: IStep; trigger: () => void }> = ({
               </label>
               <textarea
                 id="reject-comment"
-                value={rejectComment}
-                onChange={(e) => setRejectComment(e.target.value)}
+                value={approveComment}
+                onChange={(e) => setApproveComment(e.target.value)}
                 className="w-full border border-gray-200 rounded p-3 text-sm h-24 resize-none focus:outline-none focus:ring-1 focus:ring-blue-500"
                 placeholder="Enter your comments for rejection"
               />
-              {errors.rejectComment && <p className="text-red-500 text-xs mt-1">{errors.rejectComment}</p>}
+              {errors.approveComment && <p className="text-red-500 text-xs mt-1">{errors.approveComment}</p>}
             </div>
           )}
 
