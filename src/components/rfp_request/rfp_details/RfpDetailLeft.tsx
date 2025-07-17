3be6e0ec3a9bb7be48c07f11ebc8a3e600533d1b
shaanarshaqu <shaanarshaqu123@gmail.com>
2025-07-17 15:15:09 +0530

    updated on detail left

--- src/components/rfp_request/rfp_details/RfpDetailLeft.tsx
+++ src/components/rfp_request/rfp_details/RfpDetailLeft.tsx
@@ -1,16 +1,16 @@
 import React, { useEffect, useState } from "react";
 import { DocumentIcon, GeneralDetailIcon } from "../../../utils/Icons";
-import { convertCurrencyLabel, convertToAmPm } from "../../../utils/common";
+import { convertCurrencyLabel, convertToAmPm, getUserCredentials } from "../../../utils/common";
 import ShowStatus from "../../buttons/ShowStatus";
 import dayjs from "dayjs";
 import userPhoto from "../../../assets/profile_photo/userPhoto.png"
 import { getAllUsersByFilterAsync } from "../../../services/userService";
- 
+
 interface RfpDetailLeftProp {
     requestData: any | undefined
 }
- 
- 
+
+
 const CommonCard = ({ data, className }: { data: Record<string, any>, className?: string }) => {
     return (
         <div
@@ -20,7 +20,7 @@ const CommonCard = ({ data, className }: { data: Record<string, any>, className?
                 <div key={k} className="pr-[55px] group relative">
                     <p className="mb-1 text-gray-500">{k}</p>
                     <span className="block truncate">{data[k]}</span>
- 
+
                     {/* Tooltip on Hover */}
                     {data[k] && data[k]?.length > 16 && <div className="absolute left-0 top-full hidden group-hover:flex bg-[#EDF4FD] shadow-md p-2 rounded w-max max-w-[300px] z-10 border border-gray-300">
                         {data[k]}
@@ -30,7 +30,7 @@ const CommonCard = ({ data, className }: { data: Record<string, any>, className?
         </div>
     )
 }
- 
+
 type KeyValueProps = {
     data: {
         label: string;
@@ -38,7 +38,7 @@ type KeyValueProps = {
     }[];
     className: string;
 };
- 
+
 export const KeyValueGrid: React.FC<KeyValueProps> = ({ data, className }) => {
     return (
         <div className={`grid grid-cols-2 gap-y-[16px] ${className}`}>
@@ -51,17 +51,17 @@ export const KeyValueGrid: React.FC<KeyValueProps> = ({ data, className }) => {
         </div>
     );
 };
- 
+
 type UserBadge = {
     name: string;
     avatarUrl: string;
 };
- 
+
 type UserBadgesProps = {
     title: string;
     users: UserBadge[];
 };
- 
+
 export const UserBadges: React.FC<UserBadgesProps> = ({ title, users }) => {
     return (
         <div className="flex flex-col mb-[16px]">
@@ -84,11 +84,11 @@ export const UserBadges: React.FC<UserBadgesProps> = ({ title, users }) => {
         </div>
     );
 };
- 
+
 const RfpDetailLeft: React.FC<RfpDetailLeftProp> = ({ requestData }: RfpDetailLeftProp) => {
     const [rfpDocuments, setRfpDocuments] = useState<any[]>([]);
     const [owners, setOwners] = useState<{ technical: any[], commercial: any[] }>({ technical: [], commercial: [] })
- 
+
     const setupOwners = async () => {
         try {
             if (requestData) {
@@ -117,13 +117,13 @@ const RfpDetailLeft: React.FC<RfpDetailLeftProp> = ({ requestData }: RfpDetailLe
                             }
                         }
                     }
- 
+
                 })
                 setOwners(tempOwners)
             }
         } catch (err) { }
     }
- 
+
     const setDocuments = async () => {
         try {
             if (requestData) {
@@ -134,14 +134,14 @@ const RfpDetailLeft: React.FC<RfpDetailLeftProp> = ({ requestData }: RfpDetailLe
             }
         } catch (err) { }
     }
- 
+
     useEffect(() => {
         setupOwners();
         setDocuments();
     }, [requestData])
- 
+
     return (
-        <div className="h-full flex items-center bg-white flex-col px-10 pt-6 border-r border-gray-200">
+        <div className="h-full flex items-center bg-white flex-col px-10 pt-6 border-r border-gray-200 relative">
             {requestData && <>
                 {/* Project Name,ID */}
                 <div className="mb-[16px]" style={{ width: "504px" }}>
@@ -180,7 +180,7 @@ const RfpDetailLeft: React.FC<RfpDetailLeftProp> = ({ requestData }: RfpDetailLe
                         ]}
                     />
                 </div>
- 
+
                 {/* Rfp Details */}
                 <div className="h-full" style={{ width: "504px" }}>
                     <span className="font-bold text-[16px] mb-[17.5px] flex"><GeneralDetailIcon className="size-5" /><span className="pl-[8px]">Rfp Details</span></span>
@@ -204,11 +204,11 @@ const RfpDetailLeft: React.FC<RfpDetailLeftProp> = ({ requestData }: RfpDetailLe
                     <KeyValueGrid className="mb-[16px]"
                         data={[
                             { label: "Closing Date", value: dayjs(requestData?.closingDate).format("DD-MM-YYYY") },
-                            { label: "Closing Time", value:dayjs(requestData?.closingDate).format("hh:mm A") },
+                            { label: "Closing Time", value: dayjs(requestData?.closingDate).format("hh:mm A") },
                         ]}
                     />
                 </div>
- 
+
                 {/* Ownership Details */}
                 <div className="h-full" style={{ width: "504px" }}>
                     <span className="font-bold text-[16px] mb-[17.5px] flex"><GeneralDetailIcon className="size-5" /><span className="pl-[8px]">Ownership</span></span>
@@ -218,8 +218,11 @@ const RfpDetailLeft: React.FC<RfpDetailLeftProp> = ({ requestData }: RfpDetailLe
                     {rfpDocuments.length > 0 ? <div className="flex flex-col mb-[16px]">{rfpDocuments.map(d => d.attachmentComponent)}</div> : <div className="text-xs mb-[16px]">No documents found</div>}
                 </div>
             </>}
+            {requestData?.status != 1 && getUserCredentials().userId == requestData?.CreatedBy && !requestData?.isPublished && <div className="w-[504px] flex justify-end sticky bottom-2 right-0">
+                <button className="bg-customBlue h-[36px] hover:bg-blue-400 text-sm text-white rounded px-1 py-1  w-[200px]" onClick={() => {}}>Publish now</button>
+            </div>}
         </div>
     )
 }
- 
+
 export default RfpDetailLeft;
